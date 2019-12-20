8216d0c4238198ef325edd0336cf37c7238c8c9f
Arno Lehtonen <arno.lehtonen@gmail.com>
2019-12-20 18:03:49 +0200

    Fix test code style

--- spec/client.spec.js
+++ spec/client.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const MaventaBankApi = require('../index');
 require('dotenv').config();
 
@@ -8,7 +9,8 @@ describe('Maventa WDLS client tests', () => {
     const companyUuid = process.env.COMPANY_UUID;
 
     if (!vendorApiKey || !userApiKey || !companyUuid || !process.env.TEST_MESSAGE_ID) {
-      console.error(`\nApi keys are missing. Check your .env variables.\n`);
+      // eslint-disable-next-line no-console
+      console.error('\nApi keys are missing. Check your .env variables.\n');
       process.exit(1);
     }
     this.maventaClient = await new MaventaBankApi(vendorApiKey, userApiKey, companyUuid, true);
@@ -16,6 +18,7 @@ describe('Maventa WDLS client tests', () => {
 
   it('Should return Hello World', async () => {
     const res = await this.maventaClient.helloWorld();
+
     expect(res).toContain('Hello from Bank API v1.0');
   });
 
@@ -24,16 +27,19 @@ describe('Maventa WDLS client tests', () => {
       new Date(1550828295449),
       new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
     );
+
     expect(res).toEqual([]);
   });
 
   it('Should return an error on invalid message id', async () => {
     const res = await this.maventaClient.errorMessageShow('message');
+
     expect(res).toContain('ERROR: MESSAGE NOT FOUND OR NO RIGHTS');
   });
 
   it('Should try and fail to send a message', async () => {
     const res = await this.maventaClient.messageSend('message');
+
     expect(res).toBeTruthy();
   });
 
@@ -42,16 +48,19 @@ describe('Maventa WDLS client tests', () => {
       new Date(1550828295449),
       new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
     );
+
     expect(res.length).toEqual(2);
   });
 
   it('Should load a test RI-Message with id TEST_MESSAGE_ID', async () => {
     const res = await this.maventaClient.RIMessageShow(process.env.TEST_MESSAGE_ID);
+
     expect(res).toBeTruthy();
   });
 
   it('Should try to show an empty message', async () => {
     const res = await this.maventaClient.messageStatus('message');
+
     expect(res).toContain('ERROR: MESSAGE NOT FOUND OR NO RIGHTS');
   });
 });
